cb82cc98515cf0be8d57fb30ce6faccf14f4a328
Dirk-Ke <dirk.kestler@gmx.de>
2021-01-03 08:00:41 +0100

    Initial commit

---
+++ src/main/java/de/charlie/kanbanboard/Application.java
@@ -0,0 +1,22 @@
+package de.charlie.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean(servlet, "*.xhtml");
+    }
+}
