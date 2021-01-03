135b2782d27f9f5bf6f49f1f12d424dc8488eefe
Dirk-Ke <dirk.kestler@gmx.de>
2021-01-03 08:28:40 +0100

    Package 'base' added

---
+++ src/main/java/de/charlie/kanbanboard/base/repository/BaseEntity.java
@@ -0,0 +1,40 @@
+package de.charlie.kanbanboard.base.repository;
+
+import com.google.common.base.Objects;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+
+    @Version
+    private Long version;
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    @Override
+    public abstract String toString();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equal(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
